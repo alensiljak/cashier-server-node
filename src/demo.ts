b9737f4efb5c6ab30bdd7708a246d28d54065f92
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-29 14:18:45 +0100

    a very basic Node server demo

--- src/demo.ts
+++ src/demo.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import { exec } from 'child_process';
+const favicon = require('serve-favicon');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+// middleware
+
 app.use(express.json());
+// Serve the favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+
+// Methods
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.get('/test', (req, res) => {
   const { command, params } = req.body;
